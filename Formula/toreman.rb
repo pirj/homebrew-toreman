1740180bc8bcf0e91542124eb4b52c160cab5dd5
Phil Pirozhkov <hello@fili.pp.ru>
2019-05-17 17:44:49 +0300

    Update to 1.1

--- Formula/toreman.rb
+++ Formula/toreman.rb
@@ -1,7 +1,8 @@
 class Toreman < Formula
   desc "Run several processes from Procfile in Tmux splits"
   homepage "https://github.com/pirj/toreman"
-  url "https://github.com/pirj/toreman/archive/1.0.tar.gz"
+  url "https://github.com/pirj/toreman/archive/1.1.tar.gz"
+  sha256 "9c33def0b6c701bee343019a9cb42e4a2c31669b68358d79927de54518490fc1"
 
   depends_on "tmux"
 
